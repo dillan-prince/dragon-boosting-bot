9dc92a8afe98f71a1ecc661191d2ad1acfcfc7d9
Dillan Prince <dillanprince@gmail.com>
2020-10-12 20:38:05 -0500

    test

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,6 @@
 import { keys as devKeys } from './dev.js';
 import { keys as prodKeys } from './prod.js';
 
+console.log(process.env.NODE_ENV);
+
 export const keys = process.env.NODE_ENV === 'production' ? prodKeys : devKeys;
