ce8d6398d9b6baa3bde241c14175167b20fb00ed
Dillan Prince <dillanprince@gmail.com>
2020-10-17 23:42:20 -0500

    rename file

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,5 +1,5 @@
 export { addrunCommand } from './addrun.js';
 export { addCommand } from './add.js';
-export { allBalancesCommand } from './allBalances.js';
+export { allBalancesCommand } from './allbalances.js';
 export { removeCommand } from './remove.js';
 export { balanceCommand } from './balance.js';
